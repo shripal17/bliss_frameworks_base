6e82d7939c9ca47718da28fb2466c30f68110291
Sauhard Pande <sauhardp@codeaurora.org>
2016-07-21 18:23:21 -0700

    camera: Support exposing aux camera to apps
    * Replace vendor.camera.aux.packageblacklist with vendor.camera.aux.packageexcludelist
      as recommended by coding guidelines https://developers.google.com/style/word-list#blacklist.
    
    Original change:
    
    Author: Sauhard Pande <sauhardp@codeaurora.org>
    Date:   Thu Jul 21 18:23:21 2016 -0700
    
        Camera: Expose Aux camera to apps present in the whitelist
    
        Issue:
        3rd party apk cannot handle additional aux camera and may cause crash,
        Mono camera doesnt support all capabilities of HAL3 causing CTS issue.
    
        Fix:
        1. Expose aux camera to apps present in the whitelist
        2. ignore the availabe/unavailable status update for aux camera
        3. returning exception for open request for bad cameraid
    
        Change-Id: I15910154c6df205e6d4e00bfad30a00c9e3d5bee
        CRs-Fixed: 1086937
    
    ---------------------------------------------------------------------------
    
    Forward-ported to R and squashed with the following changes:
    
    Author: Rashed Abdel-Tawab <rashed@linux.com>
    Date:   Sat Mar 24 13:20:52 2018 -0400
    
        camera: Check if aux camera whitelist is set before restricting cameras
    
        Some devices have a fully featured 3rd camera and adding 20+ camera apps
        to the whitelist is impossible due to the string length limit on
        systemprops. Add a check to see if the prop is even set, and if not,
        check if the blacklist property is set and mark those apps to hide the
        3rd camera from. If a package is not part of the blacklist, just restore
        the original behaviour and expose all the cameras to the app.
    
        Change-Id: I6c3b33c077e8710c73b5d0fa28e1b017d6c43a58
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Fri Feb 8 08:42:33 2019 -0600
    
        Camera: Simplify code for Aux/Mono white and black lists
    
        Change-Id: I20c5454385d4d830a9a4d61926807a7eb1407c95
    
    Change-Id: I7eb9f78c37f06a4e713fe6bcbf1793052aad3583
    
    Change-Id: I15910154c6df205e6d4e00bfad30a00c9e3d5bee
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date: Thu, 12 Nov 2020 18:05:38 +0200
    
        CameraManager: Fixup exposing aux camera to apps
    
        Change-Id: Ic8c5844321a7310c2b72e1d7c2eceb5e3e8285e2
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: I82fb191f804f24745f832bcd21719f6f1dad47d5
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- core/java/android/hardware/camera2/CameraManager.java
+++ core/java/android/hardware/camera2/CameraManager.java
@@ -40,6 +40,7 @@ import android.content.AttributionSourceState;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
+import android.hardware.Camera;
 import android.hardware.CameraExtensionSessionStats;
 import android.hardware.CameraStatus;
 import android.hardware.ICameraService;
@@ -2350,7 +2351,10 @@ public final class CameraManager {
 
         private String[] extractCameraIdListLocked(int deviceId, int devicePolicy) {
             List<String> cameraIds = new ArrayList<>();
-            for (int i = 0; i < mDeviceStatus.size(); i++) {
+            boolean exposeAuxCamera = Camera.shouldExposeAuxCamera();
+            int size = exposeAuxCamera ? mDeviceStatus.size() : 2;
+            int idCount = 0;
+            for (int i = 0; i < size; i++) {
                 int status = mDeviceStatus.valueAt(i);
                 DeviceCameraInfo info = mDeviceStatus.keyAt(i);
                 if (status == ICameraServiceListener.STATUS_NOT_PRESENT
@@ -2926,6 +2930,11 @@ public final class CameraManager {
         }
 
         private void onStatusChangedLocked(int status, DeviceCameraInfo info) {
+            if (!Camera.shouldExposeAuxCamera() && Integer.parseInt(info.mCameraId) >= 2) {
+                Log.w(TAG, "[soar.cts] ignore the status update of camera: " + info.mCameraId);
+                return;
+            }
+
             if (DEBUG) {
                 Log.v(TAG,
                         String.format("Camera id %s has status changed to 0x%x for device %d",
