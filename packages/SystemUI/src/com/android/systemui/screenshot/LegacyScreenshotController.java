5818da6b18276983350bd932a42c0a80e05493e9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-02-26 22:16:42 +0530

    SystemUI: Screenshot: Avoid NPE while getting app label
    https://imgur.com/aY6MdSm
    
    Change-Id: I6f62ba7692e379447fac4211c46a91996ec10013
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/LegacyScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/LegacyScreenshotController.java
@@ -199,12 +199,13 @@ public class LegacyScreenshotController implements InteractiveScreenshotHandler 
     }
 
     private String getForegroundAppLabel() {
-        try {
-            final ActivityInfo ai = mPm.getActivityInfo(mTaskComponentName, 0);
-            return ai.applicationInfo.loadLabel(mPm).toString();
-        } catch (PackageManager.NameNotFoundException e) {
-             return null;
+        if (mTaskComponentName != null) {
+            try {
+                final ActivityInfo ai = mPm.getActivityInfo(mTaskComponentName, 0);
+                return ai.applicationInfo.loadLabel(mPm).toString();
+            } catch (PackageManager.NameNotFoundException e) {}
         }
+        return null;
     }
 
     @AssistedInject
