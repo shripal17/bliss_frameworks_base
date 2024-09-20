f9cbf5b34259dd1e100b4dfdd819e9e8a1cb1ecc
Jis G Jacob <studiokeys@blissroms.org>
2024-09-20 02:26:02 -0400

    Revert "Add animations to quick settings tiles [1/2]"
    This reverts commit 6a37947b66dfee33b67a5123f0f0b9c8b8e43ed1.
    
    Change-Id: I2a4c008c90c166aefe0abcf16170ad186222d31a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12870,22 +12870,6 @@ public final class Settings {
         public static final String GAME_OVERLAY = "game_overlay";
 
         /**
-         * Change quick settings tiles animation style
-         *
-         * @hide
-         */
-        public static final String QUICK_SETTINGS_TILES_ANIM_STYLE =
-                "quick_settings_tiles_anim_style";
-
-        /**
-         * Change quick settings tiles animation duration
-         *
-         * @hide
-         */
-        public static final String QUICK_SETTINGS_TILES_ANIM_DURATION =
-                "quick_settings_tiles_anim_duration";
-
-        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -285,8 +285,6 @@ public class SecureSettings {
         Settings.Secure.ON_DEVICE_INTELLIGENCE_IDLE_TIMEOUT_MS,
         Settings.Secure.MANDATORY_BIOMETRICS,
         Settings.Secure.MANDATORY_BIOMETRICS_REQUIREMENTS_SATISFIED,
-        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
-        Settings.Secure.QUICK_SETTINGS_TILES_ANIM_STYLE,
-        Settings.Secure.QUICK_SETTINGS_TILES_ANIM_DURATION
+        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS
     };
 }
