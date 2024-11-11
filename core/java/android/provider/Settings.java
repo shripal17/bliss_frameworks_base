fa87b15cbef87983ff411e13b794bc19f8e8c344
Pranav Vashi <neobuddy89@gmail.com>
2024-11-11 05:49:04 +0530

    Add support for UDFPS icons
    * This is complete rewrite basis UDFPS refactor. Cleaner and without hacks.
    
    Change-Id: I1c742bb1354fe25502e573cb6cfea3006b4614c8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6918,6 +6918,11 @@ public final class Settings {
         public static final String CLICK_PARTIAL_SCREENSHOT = "click_partial_screenshot";
 
         /**
+         * @hide
+         */
+        public static final String UDFPS_ICON = "udfps_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
